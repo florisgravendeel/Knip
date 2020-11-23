8fa36d06c2287d34d42c2507c892ce6d883d4745
Floris <florisgra@gmail.com>
2020-11-23 20:06:56 +0100

    calendar.js volledig functioneel (front-end)

--- calendar.js
+++ calendar.js
@@ -65,46 +65,93 @@ const addCSS = s =>(d=>{d.head.appendChild(d.createElement("style")).innerHTML=s
 
 
 for (i = 0; i < 80; i++){
-    document.getElementById("cell" + i).style.color = "white";
+    var cell = document.getElementById("cell" + i);
+    cell.style.color = "white";
+    cell.onclick = function () {
+        selectCell(this);
+    }
     var css = "#cell" + i + ":hover { color: black !important; }";
     addCSS(css);
 }
 
-var table = document.getElementById("agenda");
-if (table != null) {
-    for (var i = 0; i < table.rows.length; i++) {
-        for (var j = 0; j < table.rows[i].cells.length; j++)
-            table.rows[i].cells[j].onclick = function () {
-                selectCell(this);
-            };
-    }
-}
-/*
-Er mogen max. 2 cellen geselecteerd zijn. Reserveren kan met 1 tijdhok, maar ook met 2 tijdhokken.
-
- */
+/* Er mogen max. 2 cellen geselecteerd zijn. Reserveren kan met 1 tijdhok, maar ook met 2 tijdhokken. */
 function selectCell(tableCell) {
     bool = tableCell.id.includes("cell");
     if (bool) {
         if (!tableCell.classList.contains("selected") && !tableCell.classList.contains("busy")){
+            if (countSelectedCells() >= 2){
+                alert("Je kan maximaal 2 tijdvakken selecteren.");
+                return;
+            }
             tableCell.classList.add("selected");
         } else {
             tableCell.classList.remove("selected");
         }
-
-        //alert(tableCell.innerHTML + " " + bool);
+        enableButton(3, isReservationValid());
+    }
+    if (countSelectedCells() === 0){
+        enableButton(3, false);
     }
 }
 
-function getSelectedCells(){
-    var table = document.getElementById("agenda");
-    if (table != null) {
+function countSelectedCells() {
+    var count = 0;
+    for (i = 0; i < 80; i++) {
+        if (document.getElementById("cell" + i).classList.contains("selected")) {
+            count++;
+        }
+    }
+    return count;
+}
+function getSelectionCells(){
+    var list = [];
+    for (i = 0; i < 80; i++) {
+        if (document.getElementById("cell" + i).classList.contains("selected")) {
+            list.push(i);
+        }
+    }
+    return list;
+}
+function isReservationValid(){
+    var cells = getSelectionCells();
+    if (cells.length === 1){
+        return true;
+    } else if (cells.length === 2){
 
+        let cell1 = document.getElementById("cell" + cells[0]);
+        var x = cell1.innerText;
+        x = x.replaceAll(" ", "");
+        let list1 = x.split("-");
+
+        let cell2 = document.getElementById("cell" + cells[1]);
+        var y = cell2.innerText;
+        y = y.replaceAll(" ", "");
+        let list2 = y.split("-");
+
+        A = hourToMinutes(list1[0]);
+        B = hourToMinutes(list1[1]);
+        C = hourToMinutes(list2[0]);
+        D = hourToMinutes(list2[1]);
+
+        let y_axis = C - A === 30 && D - B === 30;
+        let x_axis = cells[1] - cells[0] === 5;
+        return x_axis && y_axis;
+    }
+    // Check with Http request whether there already is such a appointment
+    return false;
+}
+function hourToMinutes(string){
+    split = string.split(':');
+    if (split.length === 2){
+        return (parseInt(split[0]*60) + parseInt(split[1]));
     }
 }
 
-function makeAppointment(i){
+function makeAppointment(){
+    //getSelectionCells(); // A & B
+    console.log("Valid Reservation: " + isReservationValid());
+
+    // Is de tijd goed geselecteerd?
+
     // 2007-05-08 12:35:29 florisgra@gmail.com
-
-    console.log("number: " + i);
 }
