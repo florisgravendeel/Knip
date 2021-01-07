edf54cf7fbb2e8803acdc123e060e85fae9f8bd1
Wes <wes>
2021-01-08 00:31:44 +0100

    Homepage CSS opmaak
    Kleurenpallet toegevoegd, blokken uitgelijnd, homepage opgemaakt.

--- js/imageGallery.js
+++ js/imageGallery.js
@@ -4,3 +4,4 @@ function imageGallery(imgs) {
   let expandImg = document.getElementById("expandedImg");
   // Use the same src in the expanded image as the image being clicked on from the grid
   expandImg.src = imgs.src;
+}
