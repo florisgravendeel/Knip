da4e6b3d54531c1d7d575d642d6aa9b6a0dc14ee
Finn de Jong <Finn.dejong@student.hu.nl>
2021-01-09 15:57:46 +0100

    Behandelings keuze
    Toegevoegd:
    - 2 behandeling kiezen 
    - behandelingen display
    - dropdown arrow draait

--- js/behandelingen.js
+++ js/behandelingen.js
@@ -1,41 +1,55 @@
 // ============ Finn de Jong ============ //
 // Button functie om bepaalde behandelingen te verstoppen //
-
+document.getElementById("treatment-titles")
 function HidingKnippen(){
     var treatmentknippenlist = document.getElementById("cutting-treatments");
-    if(treatmentknippenlist.style.display === "none"){
-        treatmentknippenlist.style.display = "block"
+    if(treatmentknippenlist.style.display === "block"){
+        treatmentknippenlist.style.display = "none";
+        document.getElementById("dropdownarrow-knippen").style.transform = "rotate(-45deg)";
+        document.getElementById("dropdownarrow-knippen").style.webkitTransform = "rotate(-45deg)";
         //console.log("Hij is er!")
     }
     else{
-        treatmentknippenlist.style.display = "none"
+        treatmentknippenlist.style.display = "block";
+        document.getElementById("dropdownarrow-knippen").style.transform = "rotate(45deg)";
+        document.getElementById("dropdownarrow-knippen").style.webkitTransform = "rotate(45deg)";
         //console.log("En hij is weg!")
     }
 }
 
 function HidingVerfen(){
     var treatmentknippenlist = document.getElementById("painting-treatments");
-    if(treatmentknippenlist.style.display === "none"){
-        treatmentknippenlist.style.display = "block"
+    if(treatmentknippenlist.style.display === "block"){
+        treatmentknippenlist.style.display = "none";
+        document.getElementById("dropdownarrow-verfen").style.transform = "rotate(-45deg)";
+        document.getElementById("dropdownarrow-verfen").style.webkitTransform = "rotate(-45deg)";
         //console.log("Hij is er!")
     }
     else{
-        treatmentknippenlist.style.display = "none"
+        treatmentknippenlist.style.display = "block";
+        document.getElementById("dropdownarrow-verfen").style.transform = "rotate(45deg)";
+        document.getElementById("dropdownarrow-verfen").style.webkitTransform = "rotate(45deg)";
         //console.log("En hij is weg!")
     }
 }
 
 function HidingVerzorgen(){
     var treatmentknippenlist = document.getElementById("verzorgen-treatments");
-    if(treatmentknippenlist.style.display === "none"){
-        treatmentknippenlist.style.display = "block"
+    if(treatmentknippenlist.style.display === "block"){
+        treatmentknippenlist.style.display = "none";
+        document.getElementById("dropdownarrow-verzorgen").style.transform = "rotate(-45deg)";
+        document.getElementById("dropdownarrow-verzorgen").style.webkitTransform = "rotate(-45deg)";
         //console.log("Hij is er!")
     }
     else{
-        treatmentknippenlist.style.display = "none"
+        treatmentknippenlist.style.display = "block";
+        document.getElementById("dropdownarrow-verzorgen").style.transform = "rotate(45deg)";
+        document.getElementById("dropdownarrow-verzorgen").style.webkitTransform = "rotate(45deg)";
         //console.log("En hij is weg!")
     }
 }
+
+//      Eerste Poging
 
 // Gekozen behandeling laten zien.
 
@@ -79,103 +93,375 @@ var btn_behandeling_object = {
 };
 
 //variable
-var gekozen_behandeling = "";
+var gekozen_behandeling = [];
+
+//behandelingen displayen
+function displayTreatment() {
+    if(gekozen_behandeling.length == 0) {
+        document.getElementById("selected-treatment-display").innerHTML = "Geen";
+    }
+    else if(gekozen_behandeling.length == 1) {
+        document.getElementById("selected-treatment-display").innerHTML = gekozen_behandeling[0];
+    }
+    else if(gekozen_behandeling.length == 2) {
+        document.getElementById("selected-treatment-display").innerHTML = gekozen_behandeling[0] + " + " + gekozen_behandeling[1];
+    }   
+} 
+
+//reset de gekozen behandelingen
+var reset_button = document.getElementById("selected-treatment-reset");
+reset_button.addEventListener("click", function(){
+    gekozen_behandeling.length = 0;
+    displayTreatment();
+    console.log("reset")
+});
+
 
 //check if button is clicked
 button_id_1.addEventListener("click", function(){
-    console.log(btn_behandeling_object.btn_1);
-    gekozen_behandeling = btn_behandeling_object.btn_1;
+    var localbutton = btn_behandeling_object.btn_1
+    console.log(localbutton);
     //behandeling returnen
-    document.getElementById("selected-treatment-display").innerHTML = gekozen_behandeling;
+    if(gekozen_behandeling.length <= 2 && gekozen_behandeling.length >= 0) {
+        console.log(gekozen_behandeling.length)
+        if(gekozen_behandeling.length != 0) {
+            for(i = 0; i < gekozen_behandeling.length; i++) {
+                if (gekozen_behandeling[i] !== localbutton) {
+                    gekozen_behandeling.push(localbutton);
+                };
+            };
+        }
+        else {
+            gekozen_behandeling.push(localbutton);
+        };
+        displayTreatment();
+    };
+    console.log(gekozen_behandeling);
 });
 
 button_id_2.addEventListener("click", function(){
-    console.log(btn_behandeling_object.btn_2);
-    gekozen_behandeling = btn_behandeling_object.btn_2;
+    var localbutton = btn_behandeling_object.btn_2
+    console.log(localbutton);
     //behandeling returnen
-    document.getElementById("selected-treatment-display").innerHTML = gekozen_behandeling;
+    if(gekozen_behandeling.length <= 2 && gekozen_behandeling.length >= 0) {
+        console.log(gekozen_behandeling.length)
+        if(gekozen_behandeling.length != 0) {
+            for(i = 0; i < gekozen_behandeling.length; i++) {
+                if (gekozen_behandeling[i] !== localbutton) {
+                    gekozen_behandeling.push(localbutton);
+                };
+            };
+        }
+        else {
+            gekozen_behandeling.push(localbutton);
+        };
+        displayTreatment();
+    };
+    console.log(gekozen_behandeling);
 });
 
 button_id_3.addEventListener("click", function(){
-    console.log(btn_behandeling_object.btn_3);
-    gekozen_behandeling = btn_behandeling_object.btn_3;
+    var localbutton = btn_behandeling_object.btn_3
+    console.log(localbutton);
     //behandeling returnen
-    document.getElementById("selected-treatment-display").innerHTML = gekozen_behandeling;
+    if(gekozen_behandeling.length <= 2 && gekozen_behandeling.length >= 0) {
+        console.log(gekozen_behandeling.length)
+        if(gekozen_behandeling.length != 0) {
+            for(i = 0; i < gekozen_behandeling.length; i++) {
+                if (gekozen_behandeling[i] !== localbutton) {
+                    gekozen_behandeling.push(localbutton);
+                };
+            };
+        }
+        else {
+            gekozen_behandeling.push(localbutton);
+        };
+        displayTreatment();
+    };
+    console.log(gekozen_behandeling);
 });
 
 button_id_4.addEventListener("click", function(){
-    console.log(btn_behandeling_object.btn_4);
-    gekozen_behandeling = btn_behandeling_object.btn_4;
+    var localbutton = btn_behandeling_object.btn_4
+    console.log(localbutton);
     //behandeling returnen
-    document.getElementById("selected-treatment-display").innerHTML = gekozen_behandeling;
+    if(gekozen_behandeling.length <= 2 && gekozen_behandeling.length >= 0) {
+        console.log(gekozen_behandeling.length)
+        if(gekozen_behandeling.length != 0) {
+            for(i = 0; i < gekozen_behandeling.length; i++) {
+                if (gekozen_behandeling[i] !== localbutton) {
+                    gekozen_behandeling.push(localbutton);
+                };
+            };
+        }
+        else {
+            gekozen_behandeling.push(localbutton);
+        };
+        displayTreatment();
+    };
+    console.log(gekozen_behandeling);
 });
 
 button_id_5.addEventListener("click", function(){
-    console.log(btn_behandeling_object.btn_5);
-    gekozen_behandeling = btn_behandeling_object.btn_5;
+    var localbutton = btn_behandeling_object.btn_5
+    console.log(localbutton);
     //behandeling returnen
-    document.getElementById("selected-treatment-display").innerHTML = gekozen_behandeling;
+    if(gekozen_behandeling.length <= 2 && gekozen_behandeling.length >= 0) {
+        console.log(gekozen_behandeling.length)
+        if(gekozen_behandeling.length != 0) {
+            for(i = 0; i < gekozen_behandeling.length; i++) {
+                if (gekozen_behandeling[i] !== localbutton) {
+                    gekozen_behandeling.push(localbutton);
+                };
+            };
+        }
+        else {
+            gekozen_behandeling.push(localbutton);
+        };
+        displayTreatment();
+    };
+    console.log(gekozen_behandeling);
 });
 
 button_id_6.addEventListener("click", function(){
-    console.log(btn_behandeling_object.btn_6);
-    gekozen_behandeling = btn_behandeling_object.btn_6;
+    var localbutton = btn_behandeling_object.btn_6
+    console.log(localbutton);
     //behandeling returnen
-    document.getElementById("selected-treatment-display").innerHTML = gekozen_behandeling;
+    if(gekozen_behandeling.length <= 2 && gekozen_behandeling.length >= 0) {
+        console.log(gekozen_behandeling.length)
+        if(gekozen_behandeling.length != 0) {
+            for(i = 0; i < gekozen_behandeling.length; i++) {
+                if (gekozen_behandeling[i] !== localbutton) {
+                    gekozen_behandeling.push(localbutton);
+                };
+            };
+        }
+        else {
+            gekozen_behandeling.push(localbutton);
+        };
+        displayTreatment();
+    };
+    console.log(gekozen_behandeling);
 });
 
 button_id_7.addEventListener("click", function(){
-    console.log(btn_behandeling_object.btn_7);
-    gekozen_behandeling = btn_behandeling_object.btn_7;
+    var localbutton = btn_behandeling_object.btn_7
+    console.log(localbutton);
     //behandeling returnen
-    document.getElementById("selected-treatment-display").innerHTML = gekozen_behandeling;
+    if(gekozen_behandeling.length <= 2 && gekozen_behandeling.length >= 0) {
+        console.log(gekozen_behandeling.length)
+        if(gekozen_behandeling.length != 0) {
+            for(i = 0; i < gekozen_behandeling.length; i++) {
+                if (gekozen_behandeling[i] !== localbutton) {
+                    gekozen_behandeling.push(localbutton);
+                };
+            };
+        }
+        else {
+            gekozen_behandeling.push(localbutton);
+        };
+        displayTreatment();
+    };
+    console.log(gekozen_behandeling);
 });
 
 button_id_8.addEventListener("click", function(){
-    console.log(btn_behandeling_object.btn_8);
-    gekozen_behandeling = btn_behandeling_object.btn_8;
+    var localbutton = btn_behandeling_object.btn_8
+    console.log(localbutton);
     //behandeling returnen
-    document.getElementById("selected-treatment-display").innerHTML = gekozen_behandeling;
+    if(gekozen_behandeling.length <= 2 && gekozen_behandeling.length >= 0) {
+        console.log(gekozen_behandeling.length)
+        if(gekozen_behandeling.length != 0) {
+            for(i = 0; i < gekozen_behandeling.length; i++) {
+                if (gekozen_behandeling[i] !== localbutton) {
+                    gekozen_behandeling.push(localbutton);
+                };
+            };
+        }
+        else {
+            gekozen_behandeling.push(localbutton);
+        };
+        displayTreatment();
+    };
+    console.log(gekozen_behandeling);
 });
 
 button_id_9.addEventListener("click", function(){
-    console.log(btn_behandeling_object.btn_9);
-    gekozen_behandeling = btn_behandeling_object.btn_9;
+    var localbutton = btn_behandeling_object.btn_9
+    console.log(localbutton);
     //behandeling returnen
-    document.getElementById("selected-treatment-display").innerHTML = gekozen_behandeling;
+    if(gekozen_behandeling.length <= 2 && gekozen_behandeling.length >= 0) {
+        console.log(gekozen_behandeling.length)
+        if(gekozen_behandeling.length != 0) {
+            for(i = 0; i < gekozen_behandeling.length; i++) {
+                if (gekozen_behandeling[i] !== localbutton) {
+                    gekozen_behandeling.push(localbutton);
+                };
+            };
+        }
+        else {
+            gekozen_behandeling.push(localbutton);
+        };
+        displayTreatment();
+    };
+    console.log(gekozen_behandeling);
 });
 
 button_id_10.addEventListener("click", function(){
-    console.log(btn_behandeling_object.btn_10);
-    gekozen_behandeling = btn_behandeling_object.btn_10;
+    var localbutton = btn_behandeling_object.btn_10
+    console.log(localbutton);
     //behandeling returnen
-    document.getElementById("selected-treatment-display").innerHTML = gekozen_behandeling;
+    if(gekozen_behandeling.length <= 2 && gekozen_behandeling.length >= 0) {
+        console.log(gekozen_behandeling.length)
+        if(gekozen_behandeling.length != 0) {
+            for(i = 0; i < gekozen_behandeling.length; i++) {
+                if (gekozen_behandeling[i] !== localbutton) {
+                    gekozen_behandeling.push(localbutton);
+                };
+            };
+        }
+        else {
+            gekozen_behandeling.push(localbutton);
+        };
+        displayTreatment();
+    };
+    console.log(gekozen_behandeling);
 });
 
 button_id_11.addEventListener("click", function(){
-    console.log(btn_behandeling_object.btn_11);
-    gekozen_behandeling = btn_behandeling_object.btn_11;
+    var localbutton = btn_behandeling_object.btn_11
+    console.log(localbutton);
     //behandeling returnen
-    document.getElementById("selected-treatment-display").innerHTML = gekozen_behandeling;
+    if(gekozen_behandeling.length <= 2 && gekozen_behandeling.length >= 0) {
+        console.log(gekozen_behandeling.length)
+        if(gekozen_behandeling.length != 0) {
+            for(i = 0; i < gekozen_behandeling.length; i++) {
+                if (gekozen_behandeling[i] !== localbutton) {
+                    gekozen_behandeling.push(localbutton);
+                };
+            };
+        }
+        else {
+            gekozen_behandeling.push(localbutton);
+        };
+        displayTreatment();
+    };
+    console.log(gekozen_behandeling);
 });
 
 button_id_12.addEventListener("click", function(){
-    console.log(btn_behandeling_object.btn_12);
-    gekozen_behandeling = btn_behandeling_object.btn_12;
+    var localbutton = btn_behandeling_object.btn_12
+    console.log(localbutton);
     //behandeling returnen
-    document.getElementById("selected-treatment-display").innerHTML = gekozen_behandeling;
+    if(gekozen_behandeling.length <= 2 && gekozen_behandeling.length >= 0) {
+        console.log(gekozen_behandeling.length)
+        if(gekozen_behandeling.length != 0) {
+            for(i = 0; i < gekozen_behandeling.length; i++) {
+                if (gekozen_behandeling[i] !== localbutton) {
+                    gekozen_behandeling.push(localbutton);
+                };
+            };
+        }
+        else {
+            gekozen_behandeling.push(localbutton);
+        };
+        displayTreatment();
+    };
+    console.log(gekozen_behandeling);
 });
 
 button_id_13.addEventListener("click", function(){
-    console.log(btn_behandeling_object.btn_13);
-    gekozen_behandeling = btn_behandeling_object.btn_13;
+    var localbutton = btn_behandeling_object.btn_13
+    console.log(localbutton);
     //behandeling returnen
-    document.getElementById("selected-treatment-display").innerHTML = gekozen_behandeling;
+    if(gekozen_behandeling.length <= 2 && gekozen_behandeling.length >= 0) {
+        console.log(gekozen_behandeling.length)
+        if(gekozen_behandeling.length != 0) {
+            for(i = 0; i < gekozen_behandeling.length; i++) {
+                if (gekozen_behandeling[i] !== localbutton) {
+                    gekozen_behandeling.push(localbutton);
+                };
+            };
+        }
+        else {
+            gekozen_behandeling.push(localbutton);
+        };
+        displayTreatment();
+    };
+    console.log(gekozen_behandeling);
 });
 
 button_id_14.addEventListener("click", function(){
-    console.log(btn_behandeling_object.btn_14);
-    gekozen_behandeling = btn_behandeling_object.btn_14;
+    var localbutton = btn_behandeling_object.btn_14
+    console.log(localbutton);
     //behandeling returnen
-    document.getElementById("selected-treatment-display").innerHTML = gekozen_behandeling;
+    if(gekozen_behandeling.length <= 2 && gekozen_behandeling.length >= 0) {
+        console.log(gekozen_behandeling.length)
+        if(gekozen_behandeling.length != 0) {
+            for(i = 0; i < gekozen_behandeling.length; i++) {
+                if (gekozen_behandeling[i] !== localbutton) {
+                    gekozen_behandeling.push(localbutton);
+                };
+            };
+        }
+        else {
+            gekozen_behandeling.push(localbutton);
+        };
+        displayTreatment();
+    };
+    console.log(gekozen_behandeling);
 });
+
+
+// Ik heb hier allemaal aparte id's voor elke button
+
+
+
+//      2de Poging
+
+//  Deze poging ga ik een array makken met alle behandelingen
+
+/*
+    Wat ga ik maken?
+
+Een array met behandelingen + buttons
+For loop die behandelingen in een eventlistener zet
+
+
+*/
+/*
+var gekozen_behandeling = ""
+
+class TreatmentObject {
+    button_id;
+    treatment;
+
+    constructor(button_id, treatment) {
+        this.button_id = button_id;
+        this.treatment = treatment;
+    }
+    
+    getButtonElement(){
+        return document.getElementById(this.button_id);
+    }
+
+}
+var button_id_1 = document.getElementById("treatment-choosen-btn-1");
+var button_id_2 = document.getElementById("treatment-choosen-btn-2");
+var button_id_3 = document.getElementById("treatment-choosen-btn-3");
+
+var buttons = [new TreatmentObject("treatment-choosen-btn-1", "Knippen"), 
+               new TreatmentObject("treatment-choosen-btn-2", "Wassen, knippen en föhnen"),
+               new TreatmentObject("treatment-choosen-btn-3", "Wassen, knippen en tangen")]
+
+
+for (i=0; i < buttons.length; i++){
+    buttons[i].getButtonElement().addEventListener("click", function () {
+        console.log(buttons[i].treatment);
+        gekozen_behandeling = buttons[i].treatment;
+
+        document.getElementById("selected-treatment-display").innerHTML = gekozen_behandeling;
+    });
+    
+}
+*/
